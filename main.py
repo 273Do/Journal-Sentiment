fc77c72a63a08deaf56532913a9b09fedc63e636
273Do <keita.y310@icloud.com>
2025-10-21 12:55:54 +0000

    feat: 感情分析用のファイルを作成

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import os
 
 from dotenv import load_dotenv
 
+from src.usecase.analysis.sentiment import sentiment_analysis
 from src.usecase.data_format.csv_to_df import csv_to_df
 
 load_dotenv()
 
-print("📖 解析用にデータを整形")
+# 解析用にデータを整形
+analysis_df = csv_to_df(os.getenv("OUTPUT_PATH") + "/entry.csv")
 
-df = csv_to_df(os.getenv("OUTPUT_PATH") + "/entry.csv")
+# 感情評価データを出力
+sentiment_df = sentiment_analysis(analysis_df)
+
+# 月別感情分布(5段階)を解析
+
+
+print("====== 月別感情分布(5段階)を解析 ======")
+print("====== 月別感情バランス(5段階)を解析 ======")
---
+++ src/usecase/analysis/sentiment.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+def sentiment_analysis(analysis_df: pd.DataFrame):
+    print("🧪 感情解析処理を開始")
